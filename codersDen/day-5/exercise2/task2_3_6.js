64beb06c034d91ed93cc74c3d055806077c087d9
orjichimaprince <orjichimaprince@gmail.com>
2022-05-07 14:04:43 +0100

    arrayMethods

--- codersDen/day-5/exercise2/task2_3_6.js
+++ codersDen/day-5/exercise2/task2_3_6.js
@@ -15,3 +15,34 @@ let textNewToArray = textNew.split(' ');
 console.log(textNewToArray, );
 console.log(textNewToArray.length);
 console.log(`${textNewToArray},  '\n' ${textNewToArray.length}`);
+
+/*Que: In the following shopping cart add, remove, edit items
+
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+add 'Meat' in the beginning of your shopping cart if it has not been already added
+add Sugar at the end of you shopping cart if it has not been already added
+remove 'Honey' if you are allergic to honey
+modify Tea to 'Green Tea'*/
+
+Ans:
+    const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+shoppingCart.unshift('meat'); //adds meat in the beginning of the array.
+shoppingCart.unshift('meat')
+shoppingCart.push('Sugar');
+shoppingCart.splice(shoppingCart.length - 2, 1);
+console.log(shoppingCart);
+
+
+/*Que:Concatenate the following two variables and store it in a fullStack variable.
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+
+console.log(fullStack)
+["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]*/
+
+Ans:
+    const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node', 'Express', 'MongoDB']
+fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
